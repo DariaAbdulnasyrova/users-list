3d59ec2274def77231321439ac9260730f530d96
Daria Abdulnasyrova <daria.abdulnasyrova@gmail.com>
2025-10-18 11:56:33 +0100

    Update ci actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,6 +9,12 @@ export default defineConfig({
     headless: true,
     viewport: { width: 1280, height: 720 },
   },
+  webServer: process.env.PLAYWRIGHT_BASE_URL ? undefined : {
+    command: 'npm run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: true,
+    timeout: 120_000,
+  },
   projects: [
     { name: "Chromium", use: { ...devices["Desktop Chrome"] } },
     { name: "Firefox", use: { ...devices["Desktop Firefox"] } },
